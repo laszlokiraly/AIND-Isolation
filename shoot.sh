2d317b51dab843fbe717724f1f500ed98449d8b4
László Király <laszlo.t.kiraly@gmail.com>
2017-06-08 13:05:45 +0200

    linter, local setup

---
+++ shoot.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+echo "don't forget to: source activate aind"
+/bin/zsh -i -c 'chrome https://classroom.udacity.com/nanodegrees/nd889/parts/6be67fd1-9725-4d14-b36e-ae2b5b20804c/modules/f719d723-7ee0-472c-80c1-663f02de94f3/lessons/222105c1-630c-4726-a162-8e3380a4b67d/project'
+atom .
